c1a96a963d68514381e49380a5045c64dcc483c7
Faizan Ansari <faizansagheer346c@gmail.com>
2021-05-06 16:42:35 +0530

    added question on generic Trees

---
+++ generic-tree/1genericTreeConstructor.cpp
@@ -0,0 +1,37 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Node {
+    public:
+    int data;
+    vector<Node*> children;
+    Node(int data) {
+        this->data = data;
+    }
+};
+
+int main() {
+    int a[] = {10, 20, 50, -1, 60, -1, -1, 30, 70, -1, 80, 110, -1, 120, -1, -1, 90, -1, -1, 40, 100, -1, -1, -1};
+    int n = sizeof(a)/sizeof(a[0]);
+
+    stack<Node*> st;
+    Node* root;
+    for(int i =0; i < n; i++) {
+        if(a[i] == -1) {
+            st.pop();
+        } else {
+            Node *node = new Node(a[i]);
+            if(st.empty()) {
+                //make the node root as stack is empty
+                root = node;
+            } else {
+                //make child of previous node
+                Node* top = st.top();
+                top->children.push_back(node);
+            }
+            //finally push that node to stack
+            st.push(node);
+        }
+    }
+    return 0;
+}
